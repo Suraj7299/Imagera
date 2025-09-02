081b70a090a2c112e312b50e1d806f7eed25ec31
suraj7299 <surajbrnwal2424@gmail.com>
2025-09-02 16:01:41 +0530

    feat: initialize project with Next.js, Tailwind CSS, and essential dependencies

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home(){
+  return (
+    <main>
+      <h1>Welcome to Editly</h1>
+      <Button variant="outline">Get Started</Button>
+    </main>
+  );
+}
